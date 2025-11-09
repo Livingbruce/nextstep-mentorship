191a3d0a3198c2f170cbf17d2eb8faab01e04a7d
livingbruce <livingbruce@users.noreply.github.com>
2025-11-09 16:43:47 +0300

    fix: dynamically import backend handler for vercel api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
-const handlerModule = require('../backend/api/index.js');
+let cachedHandler;
 
-module.exports = handlerModule.default || handlerModule;
+module.exports = async function handler(req, res) {
+  if (!cachedHandler) {
+    const mod = await import('../backend/api/index.js');
+    cachedHandler = mod.default || mod;
+  }
+  return cachedHandler(req, res);
+};
 
